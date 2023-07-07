8c976361be3b491ca29396c560c32f873e4d1cf8
BenjaminTruckenbrod <btrucker94@gmail.com>
2023-07-06 22:31:03 -0400

    Added logic to place 'X' and '0' on game board

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,43 @@
+//gameModule
+
+const game = (() => {
+     let marker0 = '0';
+     let marker1= 'X';
+     let turn = 0;
+     let tiles = document.getElementsByClassName("boardTile");
+
+     const addListeners = () => {
+        for(let i=0;i<tiles.length;i++){
+            tiles[i].addEventListener("click", play)
+        }
+    }  
+
+     const play = (event) => {
+        //console.log("PLAY")
+        let val = event.srcElement.innerHTML;
+        console.log(event.srcElement.innerHTML)
+        // console.log(play.target)
+        if(turn===0 && val===''){
+            event.srcElement.innerHTML='0';
+            turn++
+        }
+        else if(turn===1 && val===''){
+            event.srcElement.innerHTML='X';
+            turn--;
+        }
+        else{
+            console.log("you can go here")
+        }
+    };
+
+    addListeners();
+
+    return {addListeners}
+}
+)();
+
+//gameBoard Module
+
 const gameBoard = (() => {
     
     let gameBoard = [['','',''],
@@ -15,31 +55,44 @@ const gameBoard = (() => {
                 x++
             }
         }
-        addListeners(tiles);
+        // addListeners(tiles);
     }
 
-    const addListeners = (t) => {
-        for(let i=0;i<t.length;i++){
-            t[i].addEventListener("click", play)
-        }
-    }   
+    // const addListeners = (t) => {
+    //     for(let i=0;i<t.length;i++){
+    //         t[i].addEventListener("click", play)
+    //     }
+    // }   
 
-    const play = (event) => {
-        //console.log("PLAY")
-        let val = event.srcElement.innerHTML;
-        console.log(event.srcElement.innerHTML)
-        // console.log(play.target)
-        if(val===''){ //&& player === 1
-            event.srcElement.innerHTML='X'
-        }
-    };
+    // const play = (event) => {
+    //     //console.log("PLAY")
+    //     let val = event.srcElement.innerHTML;
+    //     console.log(event.srcElement.innerHTML)
+    //     // console.log(play.target)
+    //     if(val===''){ //&& player === 1
+    //         event.srcElement.innerHTML='X'
+    //     }
+    // };
     return {
         addBoard
+        // addListeners
     };
 })();
 
+// Player Factory creating two players
+
+const playerFactory = (name, marker) => {
+    return {name,marker}
+}
+
+const player1 = playerFactory('Player 0', '0')
+const player2 = playerFactory('Player 1', 'X')
+
+
+// driver
 
 gameBoard.addBoard();
+game.addListeners();
 
   // make game board
   // click on tile
