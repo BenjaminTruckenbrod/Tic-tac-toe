7d8b7c2d41497d5406fd8c612dcf7eccc543b640
BenjaminTruckenbrod <btrucker94@gmail.com>
2023-07-07 10:49:43 -0400

    Rearrage and update gameplay logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,52 +1,22 @@
-//gameModule
+// Run setup after page had loaded
+window.addEventListener("load", setup);
 
-const game = (() => {
-     let marker0 = '0';
-     let marker1= 'X';
-     let turn = 0;
-     let tiles = document.getElementsByClassName("boardTile");
-
-     const addListeners = () => {
-        for(let i=0;i<tiles.length;i++){
-            tiles[i].addEventListener("click", play)
-        }
-    }  
-
-     const play = (event) => {
-        //console.log("PLAY")
-        let val = event.srcElement.innerHTML;
-        console.log(event.srcElement.innerHTML)
-        // console.log(play.target)
-        if(turn===0 && val===''){
-            event.srcElement.innerHTML='0';
-            turn++
-        }
-        else if(turn===1 && val===''){
-            event.srcElement.innerHTML='X';
-            turn--;
-        }
-        else{
-            console.log("you can go here")
-        }
-    };
-
-    addListeners();
-
-    return {addListeners}
+function setup() {
+    Board.addBoard();
+    Board.addListeners();
 }
-)();
 
 //gameBoard Module
 
-const gameBoard = (() => {
+const Board = (() => {
     
     let gameBoard = [['','',''],
                      ['','',''],
                      ['','','']];
 
-    const addBoard = () => {
-        let tiles = document.getElementsByClassName("boardTile")
+    let tiles = document.getElementsByClassName("boardTile");
 
+    const addBoard = () => {
         let x = 0; 
 
         for(let i=0; i<3; i++){
@@ -55,46 +25,130 @@ const gameBoard = (() => {
                 x++
             }
         }
-        // addListeners(tiles);
     }
 
-    // const addListeners = (t) => {
-    //     for(let i=0;i<t.length;i++){
-    //         t[i].addEventListener("click", play)
-    //     }
-    // }   
+    const updateArray = () => {
+        let x = 0;
+        for(let i=0; i<3; i++){
+            for (let j=0; j<3; j++){
+                gameBoard[i][j] = tiles[x].innerHTML;
+                x++
+            }
+        }
+
+    }
+
+    const addListeners = () => {
+        for(let i=0;i<tiles.length;i++){
+            tiles[i].addEventListener("click", Play.play)
+        }
+    }   
 
     // const play = (event) => {
-    //     //console.log("PLAY")
+
     //     let val = event.srcElement.innerHTML;
     //     console.log(event.srcElement.innerHTML)
-    //     // console.log(play.target)
+
     //     if(val===''){ //&& player === 1
     //         event.srcElement.innerHTML='X'
+    //         updateArray();
+    //         console.log(gameBoard)
     //     }
     // };
     return {
-        addBoard
-        // addListeners
+        addBoard,
+        updateArray,
+        gameBoard,
+        addListeners,
+        tiles
     };
 })();
 
+// gameModule
+
+const Play = (() => {
+     let marker0 = '0';
+     let marker1= 'X';
+     let turn = 0;
+     let tiles = Board.tiles
+
+     const play = (event) => {
+        let val = event.srcElement.innerHTML;
+        console.log(event.srcElement.innerHTML)
+        if(turn===0 && val===''){
+            event.srcElement.innerHTML='0';
+            turn++
+            updateArray()
+        }
+        else if(turn===1 && val===''){
+            event.srcElement.innerHTML='X';
+            turn--;
+        }
+        else{
+            console.log("you can't go here")
+        }
+
+        checkWin();
+    };
+
+    const updateArray = () => {
+        let x = 0;
+        for(let i=0; i<3; i++){
+            for (let j=0; j<3; j++){
+                Board.gameBoard[i][j] = tiles[x].innerHTML;
+                x++
+            }
+        }
+
+    }
+
+    const checkWin = (gameBoard) => {
+
+        let x = ''
+        let y = ''
+        let counter = 0;
+    
+        for(let i=0; i<3; i++){
+            for (let j=0; j<3; j++){
+                x = gameBoard[i][j]
+                y = gameBoard[i][j+1]
+    
+                if(x!=y){
+                    counter=0
+                }
+                if(x==y && counter==0){
+                    counter++;
+                }
+                if(x==y && counter==1){
+                    console.log("Winner")
+                    //break
+                }
+            }
+        }
+        if (gameBoard[0][0]== gameBoard[0][1]== gameBoard[0][2]){
+            console.log("Win")
+        }
+        if (gameBoard[0][0]== gameBoard[0][1]== gameBoard[0][2]){
+            console.log("Win")
+        }
+    }
+
+
+    return {play}
+}
+)();
+
 // Player Factory creating two players
 
-const playerFactory = (name, marker) => {
-    return {name,marker}
-}
+// const playerFactory = (name, marker) => {
+//     return {name,marker}
+// }
 
-const player1 = playerFactory('Player 0', '0')
-const player2 = playerFactory('Player 1', 'X')
+// const player1 = playerFactory('Player 0', '0')
+// const player2 = playerFactory('Player 1', 'X')
 
 
 // driver
+// gameBoard.addBoard();
+// game.addListeners();
 
-gameBoard.addBoard();
-game.addListeners();
-
-  // make game board
-  // click on tile
-    // if tile is a 0, change tile to x
-        //target.value
